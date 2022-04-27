1ef4e5d55fa981ccb2e2c8f1cfb8a4fe5f98dcc0
이건 <akstp88@gmail.com>
2022-04-27 15:37:50 +0900

    initial commit

---
+++ hamlet-backend/src/main/java/com/hamlet/HamletBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hamlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamletBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamletBackendApplication.class, args);
+	}
+
+}
