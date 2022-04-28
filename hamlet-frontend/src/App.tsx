43ffea46a03a47404055b9385f555b0f8e2d414e
JongKyuHong <kongi7064@gmail.com>
2022-04-28 14:18:44 +0900

    initial commit

--- hamlet-frontend/src/App.tsx
+++ hamlet-frontend/src/App.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 // import logo from './logo.png';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './components/Main/main';
-import Login from './components/Auth/login';
-import Signup from './components/Auth/signup';
+import Main from './pages/Main/main';
+import Login from './pages/LogIn/login';
+import Signup from './pages/SignUp/signup';
 
 function App() {
   return (
